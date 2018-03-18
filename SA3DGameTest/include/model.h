dcfedda614cbc4e089003903ae3157e288f3c42c
sma69 <sma69@njit.edu>
2018-03-18 18:24:20 -0400

    added stb image header

---
+++ SA3DGameTest/include/model.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <GL/glew.h>
+
+#include "shader.h"
+#include "graphics.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "stb_image.h"
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
