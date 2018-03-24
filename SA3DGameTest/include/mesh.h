ea1ff88b573090223ba11b653e426b4f782d22f4
sma69 <sma69@njit.edu>
2018-03-23 23:52:42 -0400

    added model class

---
+++ SA3DGameTest/include/mesh.h
@@ -0,0 +1,69 @@
+#ifndef __MESH_H__
+#define __MESH_H__
+
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <GL/glew.h>
+
+#include "shader.h"
+#include "graphics.h"
+
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace sa3d {
+	namespace graphics {
+
+
+		/**
+		@brief Vertex buffer class to create triangles for mesh 
+		**/
+		struct Vertex {
+			glm::vec3 Position;
+			glm::vec3 Normal;
+			glm::vec2 TexCoords;
+		};
+
+		/**
+		@brief  Texture class take getting Image path and file type of image file
+		**/
+
+		struct Texture {
+			GLuint id;
+			std::string type;
+			aiString path;
+		};
+
+		/**
+		@brief Mesh class for creating object from file
+		*/
+		class Mesh {
+		public:
+			/*  Mesh Data  */
+			std::vector<Vertex> vertices;
+			std::vector<GLuint> indices;
+			std::vector<Texture> textures;
+			/*  Functions  */
+			/**Constructor */
+			Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures);
+			
+			/** Draw mesh from file*/
+			void Draw(Shader shader);
+		private:
+			/*  Render data  */
+			GLuint VAO, VBO, EBO;
+			/*  Functions    */
+			void setupMesh();
+		};
+
+	}
+}
+#endif
--- SA3DGameTest/include/model.h
+++ SA3DGameTest/include/model.h
@@ -15,3 +15,34 @@
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+
+
+namespace sa3d {
+	namespace graphics {
+
+		GLint TextureFromFile(const char* path, std::string directory);
+
+		class Model
+		{
+		public:
+			/**  Functions   */
+			/**Constructor*/
+			Model(GLchar* path);
+
+			void Draw(Shader shader);
+		private:
+			/**  Model Data  */
+			std::vector<Mesh> meshes;
+			std::string directory;
+			std::vector<Texture> textures_loaded;	// Stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+
+													/*  Functions   */
+			void loadModel(std::string path);
+			void processNode(aiNode* node, const aiScene* scene);
+			Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+			std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+				std::string typeName);
+
+		};
+	}
+}
